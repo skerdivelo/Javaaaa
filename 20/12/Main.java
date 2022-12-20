79e7e771232fc5cfe7a42a632aa43d5e8dfb24c3
Skerdi <81383971+skerdivelo@users.noreply.github.com>
2022-12-20 11:29:31 +0000

    asd

---
+++ 20/12/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        //li chiamo con il nome della classe poichè sono statici
+        Scanner s = new Scanner(System.in);
+        MultaTutor.setVelocitàLimite(2.);
+        MultaTutor.setSpazio(45.);
+        MultaTutor.setMulta(5);
+        MultaTutor m1 = new MultaTutor(" ", 0);
+        MultaTutor m2 = new MultaTutor(" ", 0);
+        MultaTutor m3 = new MultaTutor(" ", 0);
+        String targa;
+        double tempo;
+        //inserisco i dati della prima macchina
+        System.out.println("Inserisci la targa della macchina: ");
+        targa = s.nextLine();
+        m1.setTarga(targa);
+        System.out.println("Inserisci il tempo di percorrenza della macchina: ");
+        tempo = s.nextDouble();
+        m1.setTempo(tempo);
+        //inserisco i dati della seconda macchina
+        System.out.println("Inserisci la targa della macchina: ");
+        targa = s.nextLine();
+        m2.setTarga(targa);
+        System.out.println("Inserisci il tempo di percorrenza della macchina: ");
+        tempo = s.nextDouble();
+        m2.setTempo(tempo);
+        //inserisco i dati della terza macchina
+        System.out.println("Inserisci la targa della macchina: ");
+        targa = s.nextLine();
+        m3.setTarga(targa);
+        System.out.println("Inserisci il tempo di percorrenza della macchina: ");
+        tempo = s.nextDouble();
+        m3.setTempo(tempo);
+
+
+
+        //verifico se è da multare
+        if(m1.multare()){
+            System.out.println("La macchina con targa: " + m1.getTarga() + " deve essere multata");
+        }else{
+            System.out.println("Nulla da multare");
+        }
+        System.out.println("La velocità della macchina: " + m2.getTarga() + " è di: " + m2.calcoloVelocita());
+        if(m3.multare()){
+            System.out.println("La macchina: " + m3.getTarga()+ " deve pagare: " +m3.importoMulta());
+        }
+        
+    }   
+}
---
+++ 20/12/MultaTutor.java
@@ -0,0 +1,82 @@
+public class MultaTutor{
+    private String targa;
+    private double tempo;
+    private static double spazio;
+    private static double velocitàLimite; 
+    private static double multa; // multa sarebbe l'importo che uso per calcolarmi quanto devo pagare dopo aver superato il limite di velocità
+
+    //costrutore
+    public MultaTutor(String targa, double tempo){
+        this.targa = targa;
+        this.tempo = tempo;
+    }
+
+    //metodi get
+    public String getTarga() {
+        return targa;
+    }
+    public double getTempo() {
+        return tempo;
+    }
+    public static double getSpazio() {
+        return spazio;
+    }
+    public static double getVelocitàLimite() {
+        return velocitàLimite;
+    }
+    public static double getMulta() {
+        return multa;
+    }
+
+    //metodi set
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+    public void setTempo(double tempo) {
+        if(tempo<=0){
+            System.out.println("Il tempo non può essere negativo o uguale a 0");
+        }else{
+            this.tempo = tempo;
+        }
+    }
+    public static void setSpazio(double spazio) {
+        MultaTutor.spazio = spazio;
+    }
+    public static void setVelocitàLimite(double velocitàLimite) {
+        MultaTutor.velocitàLimite = velocitàLimite;
+    }
+    public static void setMulta(double multa) {
+        MultaTutor.multa = multa;
+    }
+
+    //calcolo la velocità facendo spazio/tempo
+    public double calcoloVelocita(){
+        double v = spazio/tempo;
+        return v;
+    }
+    //metodo che mi serve per capire se un auto è da multare
+    public boolean multare(){
+        if(calcoloVelocita()>velocitàLimite){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public double importoMulta(){
+        //faccio la differenza fra la velocità e il limite
+        //ad esempio se il limite è 50 e sto andando a 54, 54-50=4;
+        double kmEcc = calcoloVelocita()-velocitàLimite;
+        double multaPagare;
+        //controllo la sottrazione, cioè se è negativo non dico che l'utente deve pagare -180
+        if(kmEcc<=0){
+            multaPagare = 0;
+            return multaPagare;
+        }else{
+            multaPagare = multa*kmEcc;
+            return multaPagare;
+        }
+        //qua faccio la multa. Cioè il mio attributo multa vale 20 e se io supero di 4 faccio: 20*4
+        
+    }
+}
